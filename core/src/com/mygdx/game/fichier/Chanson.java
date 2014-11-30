08b0e942b1a2fc2c2259bbcdc5febd67041f26ba
RevJam <jamu.sandra@gmail.com>
2014-11-30 11:53:55 +0100

    Validation

--- core/src/com/mygdx/game/fichier/Chanson.java
+++ core/src/com/mygdx/game/fichier/Chanson.java
@@ -10,7 +10,7 @@ public class Chanson {
     private String title;
     private List<Note> listNote = new ArrayList<Note>();
 
-    public Chanson(String title, int difficulty, List<Note> listNote) {
+    public Chanson(String title, List<Note> listNote) {
         this.title = title;
         this.listNote = listNote;
     }
@@ -34,5 +34,31 @@ public class Chanson {
 
     public void setListNote(List<Note> listNote) {
         this.listNote = listNote;
+    }
+
+    /* temps: moment où le joueur appuie,
+        position: note sur lequel le joueur appuie,
+        duree: temps entre le push et le up du joueur sur la note
+        difficultyTime: plage acceptable de temps ex: la note est a 42ms, difficultytime=3ms, temps=41ms => OKAY
+     */
+    public boolean validation(int temps, int position, int duree, int difficultyTime){
+        Note n= null;
+        for(int i = 0; i<listNote.size(); i++){
+            n = listNote.get(i);
+            if (n.getTemps()< temps+difficultyTime && n.getTemps()>temps-difficultyTime){
+                if (n.getPosition() == position){
+                    if (n.getDuree() < duree+difficultyTime && n.getDuree()>duree-difficultyTime){
+                        // On retire de la liste la note et ses predecesseurs (note raté). La note est toujours une des premiere trouvée.
+                        for (int j=0; j<i+1; j++){
+                            listNote.remove(j);
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }
+        return false;
     }
 }
