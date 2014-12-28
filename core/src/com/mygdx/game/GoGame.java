359abe62488ec757d9626b8d254630a2451abe4d
juliette <ju@ju.com>
2014-12-28 23:26:53 +0100

    gogame

--- core/src/com/mygdx/game/GoGame.java
+++ core/src/com/mygdx/game/GoGame.java
@@ -2,14 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.mygdx.game.fichier.Lecture;
 
 import java.io.IOException;
 
@@ -21,72 +15,66 @@ import java.io.IOException;
 public class GoGame implements Screen {
     MyGdxGame game;
     Texture tex;
-    SpriteBatch batch;
-    int hpos, vpos, diff;
-    int tailleBarre, score;
+
 
     public GoGame(MyGdxGame game, Texture tex) throws IOException {
         this.game = game;
         this.tex = tex;
-        batch = new SpriteBatch();
-        diff = 5;
-        tailleBarre = game.getLongueur()/15;
-        hpos = game.getLongueur() - tex.getHeight() - tailleBarre;
-        vpos = game.getLargeur()/2 - tex.getHeight();
-        score = 0;
+
     }
 
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
 
-        hpos -= (int) (Gdx.input.getAccelerometerY())*diff;
-        vpos -= (int) (Gdx.input.getAccelerometerX())*diff;
-
-        if (hpos < 0) {
-            hpos = 0;
-        }
-
-        if (hpos > Gdx.graphics.getHeight() - tex.getHeight() - tailleBarre)
-            hpos = Gdx.graphics.getHeight() - tex.getHeight() - tailleBarre;
-
-        if (vpos < 0) {
-            vpos=0;
-        }
-
-        if (vpos > Gdx.graphics.getWidth() - tex.getWidth()) {
-            vpos = Gdx.graphics.getWidth() - tex.getWidth();
-        }
-
-        hpos += (int)(Gdx.graphics.getDeltaTime());
-        vpos += (int)(Gdx.graphics.getDeltaTime());
-
-        batch.draw(tex,vpos,hpos);
-        batch.end();
     }
 
+    /**
+     * @param width
+     * @param height
+     */
+    @Override
     public void resize(int width, int height) {
 
     }
 
+    /**
+     * Called when this screen becomes the current screen .
+     */
+    @Override
     public void show() {
 
     }
 
+    /**
+     * Called when this screen is no longer the current screen
+     */
+    @Override
     public void hide() {
 
     }
 
+    /**
+     *
+     */
+    @Override
     public void pause() {
 
     }
 
+    /**
+     *
+     */
+    @Override
     public void resume() {
 
     }
 
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
     public void dispose() {
 
     }
