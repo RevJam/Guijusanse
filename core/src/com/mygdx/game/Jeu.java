7fb0f8d3d221ea30613b3f702fad1945c7a79a85
juliette <ju@ju.com>
2014-12-29 17:43:57 +0100

    que 3 boutons

--- core/src/com/mygdx/game/Jeu.java
+++ core/src/com/mygdx/game/Jeu.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +24,8 @@ public class Jeu implements Screen {
     Stage stage;
     List<TextButton> difficulty;
 
+    TextButton button1,button2,button3;
     public Jeu(MyGdxGame game) {
-        TextButton button1,button2,button3;
 
         myGdxGame=game;
 
@@ -80,6 +82,24 @@ public class Jeu implements Screen {
      */
     @Override
     public void show() {
+        button1.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+        button2.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+        button3.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
 
     }
 
