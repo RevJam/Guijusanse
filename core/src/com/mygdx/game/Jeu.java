123e81efb7b228373f001ad1deb99b4092a0c4c3
sebastien <sebastien@thunderbolt.com>
2014-12-30 18:22:26 +0100

    tentative d'anim

--- core/src/com/mygdx/game/Jeu.java
+++ core/src/com/mygdx/game/Jeu.java
@@ -138,48 +138,46 @@ public class Jeu implements Screen {
 
         //Crée la liste d'image correspondant au note.
 
+
         for(Note n: chanson.getListNote()){
             note = n;
-            timer.scheduleTask(new Timer.Task() {
+            System.out.println(note.toString());
+
+            img = null;
+            moveAction = new MoveToAction();
+            moveAction.setDuration(vitesse);
+
+            if (note.getPosition() == 0) {
+                img = new Image(new Texture("skin/bB.png"));
+                img.setPosition(150, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(150f, -300f);
+            } else if (note.getPosition() == 1) {
+                img = new Image(new Texture("skin/bA.png"));
+                img.setPosition(450, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(450f, -300f);
+            } else if (note.getPosition() == 2) {
+                img = new Image(new Texture("skin/bC.png"));
+                img.setPosition(750, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(750f, -300f);
+            }
+            img.setSize(150, 150);
+            img.addListener(new ClickListener() {
                 @Override
-                public void run() {
-                    img = null;
-                    moveAction = new MoveToAction();
-                    moveAction.setDuration(vitesse);
-
-                    if (note.getPosition() == 0) {
-                        img = new Image(new Texture("skin/bB.png"));
-                        img.setPosition(150, myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(150f, -300f);
-                    }else if (note.getPosition() == 1) {
-                        img = new Image(new Texture("skin/bA.png"));
-                        img.setPosition(450,myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(450f, -300f);
-                    }else if (note.getPosition() == 2) {
-                        img = new Image(new Texture("skin/bC.png"));
-                        img.setPosition(750, myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(750f, -300f);
+                public void clicked(InputEvent event, float x, float y) {
+                    if (img.getCenterX() == button1.getCenterX()) {
+                        System.out.print(" image 1 11  1 1 1");
                     }
-                    img.setSize(150, 150);
-                    img.addListener(new ClickListener(){
-                        @Override
-                        public void clicked(InputEvent event, float x, float y) {
-                            if(img.getCenterX()==button1.getCenterX()){
-                                System.out.print(" image 1 11  1 1 1");
-                            }
-                            if(img.getCenterX()==button2.getCenterX()){
-                                System.out.print(" image 2222222222222");
-                            }
-                            if(img.getCenterX()==button3.getCenterX()){
-                                System.out.print(" image333333333333333333333");
-                            }
-                        }
-
-                    });
-                    img.addAction(moveAction);
-                    stage.addActor(img);
+                    if (img.getCenterX() == button2.getCenterX()) {
+                        System.out.print(" image 2222222222222");
+                    }
+                    if (img.getCenterX() == button3.getCenterX()) {
+                        System.out.print(" image333333333333333333333");
+                    }
                 }
-            },note.getTemps());
+
+            });
+            img.addAction(moveAction);
+            stage.addActor(img);
         }
 
     }
