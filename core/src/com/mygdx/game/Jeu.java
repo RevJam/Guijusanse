79362bd0b88491544e0644ac3518b547f554b420
juliette <ju@ju.com>
2014-12-30 18:30:15 +0100

    recherche du défilement des notes

--- core/src/com/mygdx/game/Jeu.java
+++ core/src/com/mygdx/game/Jeu.java
@@ -34,7 +34,7 @@ public class Jeu implements Screen {
     float vitesse;
     Thread t;
     private Music music;
-    TextButton button1,button2,button3;
+    TextButton button1, button2, button3;
     List<Note> listNote;
     Image img;
     Note note;
@@ -43,38 +43,38 @@ public class Jeu implements Screen {
 
     public Jeu(MyGdxGame game) {
         timer = new Timer();
-        myGdxGame=game;
-        table=new Table();
+        myGdxGame = game;
+        table = new Table();
         stage = new Stage();
         vitesse = 5f;
         // Charge le skin de l'appli
-        skin =new Skin(Gdx.files.internal("skin/defaultskin.json"),new TextureAtlas(Gdx.files.internal("skin/default.pack")));
+        skin = new Skin(Gdx.files.internal("skin/defaultskin.json"), new TextureAtlas(Gdx.files.internal("skin/default.pack")));
 
-        button1 = new TextButton(null,skin,"buttontwo");
+        button1 = new TextButton(null, skin, "buttontwo");
         button1.setSize(150, 150);
         button1.setPosition(150, 100);
         stage.addActor(button1);
 
-        button2 = new TextButton(null,skin,"buttonthree");
-        button2.setSize(150,150);
-        button2.setPosition(450,100);
+        button2 = new TextButton(null, skin, "buttonthree");
+        button2.setSize(150, 150);
+        button2.setPosition(450, 100);
         stage.addActor(button2);
 
 
-        button3 = new TextButton(null,skin,"buttonfour");
-        button3.setSize(150,150);
+        button3 = new TextButton(null, skin, "buttonfour");
+        button3.setSize(150, 150);
         button3.setPosition(750, 100);
         stage.addActor(button3);
-        t= new Thread() {
+        t = new Thread() {
             public void run() {
-                stage.addListener(new ClickListener(){
+                stage.addListener(new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
-                        if(button1.isPressed())
+                        if (button1.isPressed())
                             System.out.println("1");
-                        if(button2.isPressed())
+                        if (button2.isPressed())
                             System.out.println("2");
-                        if(button3.isPressed())
+                        if (button3.isPressed())
                             System.out.println("3");
                     }
                 });
@@ -130,56 +130,56 @@ public class Jeu implements Screen {
 
         // On place la scène
         Gdx.input.setInputProcessor(stage);
-        String s=chanson.getTitle().replaceAll("[\\W]", "");
-        music=Gdx.audio.newMusic(Gdx.files.internal("sound/"+ s+".mp3"));
+        String s = chanson.getTitle().replaceAll("[\\W]", "");
+        music = Gdx.audio.newMusic(Gdx.files.internal("sound/" + s + ".mp3"));
         music.setLooping(true);
         music.setVolume(0.5f);
         music.play();
 
         //Crée la liste d'image correspondant au note.
 
-        for(Note n: chanson.getListNote()){
+
+        for (Note n : chanson.getListNote()) {
             note = n;
-            timer.scheduleTask(new Timer.Task() {
+            System.out.println(note.toString());
+
+            img = null;
+            moveAction = new MoveToAction();
+            moveAction.setDuration(vitesse);
+
+            if (note.getPosition() == 0) {
+                img = new Image(new Texture("skin/bB.png"));
+                img.setPosition(150, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(150f, -300f);
+
+            } else if (note.getPosition() == 1) {
+                img = new Image(new Texture("skin/bA.png"));
+                img.setPosition(450, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(450f, -300f);
+
+            } else if (note.getPosition() == 2) {
+                img = new Image(new Texture("skin/bC.png"));
+                img.setPosition(750, myGdxGame.getLongueur() + vitesse * 50 + note.getTemps());
+                moveAction.setPosition(750f, -300f);
+            }
+            img.setSize(150, 150);
+            img.addListener(new ClickListener() {
                 @Override
-                public void run() {
-                    img = null;
-                    moveAction = new MoveToAction();
-                    moveAction.setDuration(vitesse);
-
-                    if (note.getPosition() == 0) {
-                        img = new Image(new Texture("skin/bB.png"));
-                        img.setPosition(150, myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(150f, -300f);
-                    }else if (note.getPosition() == 1) {
-                        img = new Image(new Texture("skin/bA.png"));
-                        img.setPosition(450,myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(450f, -300f);
-                    }else if (note.getPosition() == 2) {
-                        img = new Image(new Texture("skin/bC.png"));
-                        img.setPosition(750, myGdxGame.getLongueur()+ vitesse*50);
-                        moveAction.setPosition(750f, -300f);
+                public void clicked(InputEvent event, float x, float y) {
+                    if (img.getCenterX() == button1.getCenterX()) {
+                        System.out.print(" image 1 11  1 1 1");
                     }
-                    img.setSize(150, 150);
-                    img.addListener(new ClickListener(){
-                        @Override
-                        public void clicked(InputEvent event, float x, float y) {
-                            if(img.getCenterX()==button1.getCenterX()){
-                                System.out.print(" image 1 11  1 1 1");
-                            }
-                            if(img.getCenterX()==button2.getCenterX()){
-                                System.out.print(" image 2222222222222");
-                            }
-                            if(img.getCenterX()==button3.getCenterX()){
-                                System.out.print(" image333333333333333333333");
-                            }
-                        }
-
-                    });
-                    img.addAction(moveAction);
-                    stage.addActor(img);
+                    if (img.getCenterX() == button2.getCenterX()) {
+                        System.out.print(" image 2222222222222");
+                    }
+                    if (img.getCenterX() == button3.getCenterX()) {
+                        System.out.print(" image333333333333333333333");
+                    }
                 }
-            },note.getTemps());
+
+            });
+            img.addAction(moveAction);
+            stage.addActor(img);
         }
 
     }
