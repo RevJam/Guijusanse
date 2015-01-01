ef08d0682a09df199ce539dd3e926d644c07b455
juliette <ju@ju.com>
2015-01-02 00:34:44 +0100

    j'ai enlevé le thread pour les listener des boutons, qui ne servent a rien mnt

--- core/src/com/mygdx/game/Jeu.java
+++ core/src/com/mygdx/game/Jeu.java
@@ -32,56 +32,55 @@ public class Jeu implements Screen {
     Stage stage;
     Chanson chanson;
     float vitesse;
-    Thread t;
     private Music music;
-    TextButton button1, button2, button3;
+    TextButton button1,button2,button3;
     List<Note> listNote;
     Image img;
     Note note;
     MoveToAction moveAction;
-    Timer timer;
+
+    int count;
+    long timeCount;
 
     public Jeu(MyGdxGame game) {
-        timer = new Timer();
-        myGdxGame = game;
-        table = new Table();
+        myGdxGame=game;
+        table=new Table();
         stage = new Stage();
+        count = 0;
+        timeCount = 0;
         vitesse = 5f;
         // Charge le skin de l'appli
-        skin = new Skin(Gdx.files.internal("skin/defaultskin.json"), new TextureAtlas(Gdx.files.internal("skin/default.pack")));
+        skin =new Skin(Gdx.files.internal("skin/defaultskin.json"),new TextureAtlas(Gdx.files.internal("skin/default.pack")));
 
-        button1 = new TextButton(null, skin, "buttontwo");
+        button1 = new TextButton(null,skin,"buttontwo");
         button1.setSize(150, 150);
         button1.setPosition(150, 100);
         stage.addActor(button1);
 
-        button2 = new TextButton(null, skin, "buttonthree");
-        button2.setSize(150, 150);
-        button2.setPosition(450, 100);
+        button2 = new TextButton(null,skin,"buttonthree");
+        button2.setSize(150,150);
+        button2.setPosition(450,100);
         stage.addActor(button2);
 
 
-        button3 = new TextButton(null, skin, "buttonfour");
-        button3.setSize(150, 150);
+        button3 = new TextButton(null,skin,"buttonfour");
+        button3.setSize(150,150);
         button3.setPosition(750, 100);
         stage.addActor(button3);
-        t = new Thread() {
-            public void run() {
-                stage.addListener(new ClickListener() {
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        if (button1.isPressed())
-                            System.out.println("1");
-                        if (button2.isPressed())
-                            System.out.println("2");
-                        if (button3.isPressed())
-                            System.out.println("3");
-                    }
-                });
 
+        stage.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(button1.isPressed())
+                    System.out.println("1");
+                if(button2.isPressed())
+                    System.out.println("2");
+                if(button3.isPressed())
+                    System.out.println("3");
             }
-        };
-        t.start();
+        });
+
+
     }
 
     /**
@@ -94,10 +93,48 @@ public class Jeu implements Screen {
         // On colore le fond
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        note = listNote.get(count);
+        //  System.out.println(delta);
 
-        //Couper la musique
-        myGdxGame.getMusic().stop();
+        if(timeCount == note.getTemps()) {
+            img = null;
+            moveAction = new MoveToAction();
+            moveAction.setDuration(vitesse);
 
+            if (note.getPosition() == 0) {
+                img = new Image(new Texture("skin/bB.png"));
+                img.setPosition(150, myGdxGame.getLongueur() + vitesse * note.getDuree() + note.getTemps());
+                moveAction.setPosition(150f, -300f + note.getTemps());
+            } else if (note.getPosition() == 1) {
+                img = new Image(new Texture("skin/bA.png"));
+                img.setPosition(450, myGdxGame.getLongueur() + vitesse * note.getDuree() + note.getTemps());
+                moveAction.setPosition(450f, -300f + note.getTemps());
+            } else if (note.getPosition() == 2) {
+                img = new Image(new Texture("skin/bC.png"));
+                img.setPosition(750, myGdxGame.getLongueur() + vitesse * note.getDuree() + note.getTemps());
+                moveAction.setPosition(750f, -300f + note.getTemps());
+            }
+            img.setSize(150, 150);
+            img.addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if (img.getCenterX() == button1.getCenterX()) {
+                        System.out.print(" image 1 11  1 1 1");
+                    }
+                    if (img.getCenterX() == button2.getCenterX()) {
+                        System.out.print(" image 2222222222222");
+                    }
+                    if (img.getCenterX() == button3.getCenterX()) {
+                        System.out.print(" image333333333333333333333");
+                    }
+                }
+
+            });
+            img.addAction(moveAction);
+            stage.addActor(img);
+            count++;
+        }
+        //System.out.println("toto");
         // On lance la scene et la met en visible
         stage.act();
         stage.draw();
@@ -118,6 +155,11 @@ public class Jeu implements Screen {
      */
     @Override
     public void show() {
+
+
+        //Couper la musique
+        myGdxGame.getMusic().stop();
+
         table.setFillParent(true);
         //On ajoute les acteurs a la scène
         stage.addActor(table);
@@ -130,57 +172,21 @@ public class Jeu implements Screen {
 
         // On place la scène
         Gdx.input.setInputProcessor(stage);
-        String s = chanson.getTitle().replaceAll("[\\W]", "");
-        music = Gdx.audio.newMusic(Gdx.files.internal("sound/" + s + ".mp3"));
-        music.setLooping(false);
+        String s=chanson.getTitle().replaceAll("[\\W]", "");
+        music=Gdx.audio.newMusic(Gdx.files.internal("sound/"+ s+".mp3"));
+        music.setLooping(true);
         music.setVolume(0.5f);
         music.play();
 
         //Crée la liste d'image correspondant au note.
 
+        listNote = chanson.getListNote();
 
-        for (Note n : chanson.getListNote()) {
-            note = n;
-            System.out.println(note.toString());
 
-            img = null;
-            moveAction = new MoveToAction();
-            moveAction.setDuration(vitesse);
+        //lancer la prochaine note lorsque le timet
 
-            if (note.getPosition() == 0) {
-                img = new Image(new Texture("skin/bB.png"));
-                img.setPosition(150, myGdxGame.getLongueur() + vitesse * note.getDuree() + note.getTemps());
-                moveAction.setPosition(150f, -300f-note.getTemps());
 
-            } else if (note.getPosition() == 1) {
-                img = new Image(new Texture("skin/bA.png"));
-                img.setPosition(450, myGdxGame.getLongueur() + vitesse * note.getDuree() + note.getTemps());
-                moveAction.setPosition(450f, -300f-note.getTemps());
 
-            } else if (note.getPosition() == 2) {
-                img = new Image(new Texture("skin/bC.png"));
-                img.setPosition(750, myGdxGame.getLongueur() + vitesse *note.getDuree() + note.getTemps());
-                moveAction.setPosition(750f, -300f-note.getTemps());
-            }
-            img.setSize(150, 150);
-            img.addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-                    if (img.getCenterX() == button1.getCenterX()) {
-                        System.out.print(" image 1 11  1 1 1");
-                    }
-                    if (img.getCenterX() == button2.getCenterX()) {
-                        System.out.print(" image 2222222222222");
-                    }
-                    if (img.getCenterX() == button3.getCenterX()) {
-                        System.out.print(" image333333333333333333333");
-                    }
-                }
-
-            });
-            img.addAction(moveAction);
-            stage.addActor(img);
-        }
     }
 
     /**
@@ -188,7 +194,6 @@ public class Jeu implements Screen {
      */
     @Override
     public void hide() {
-        t.interrupt();
         music.pause();
     }
 
@@ -197,7 +202,6 @@ public class Jeu implements Screen {
      */
     @Override
     public void pause() {
-        t.interrupt();
         music.pause();
     }
 
@@ -206,7 +210,7 @@ public class Jeu implements Screen {
      */
     @Override
     public void resume() {
-        t.interrupt();
+
         music.pause();
     }
 
