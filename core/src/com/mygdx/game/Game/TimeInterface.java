092c2e2fa5731559e1761cbe7d98d0d1e8961714
sebastien <sebastien@thunderbolt.com>
2015-01-02 17:18:26 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	core/src/com/mygdx/game/Jeu.java

---
+++ core/src/com/mygdx/game/Game/TimeInterface.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.Game;
+
+/**
+ * Created by sebastien on 02/01/15.
+ */
+public interface TimeInterface {
+
+
+    public void startTime();
+
+    public long getTime();
+
+
+}
