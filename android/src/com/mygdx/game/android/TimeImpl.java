aa316e740052d5caff8a0641dc6ad4ef20258af0
sebastien <sebastien@thunderbolt.com>
2015-01-02 17:18:07 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	core/src/com/mygdx/game/Jeu.java

---
+++ android/src/com/mygdx/game/android/TimeImpl.java
@@ -0,0 +1,38 @@
+package com.mygdx.game.android;
+
+import com.mygdx.game.Game.TimeInterface;
+
+import static android.os.SystemClock.currentThreadTimeMillis;
+import static android.os.SystemClock.uptimeMillis;
+
+/**
+ * Created by sebastien on 02/01/15.
+ */
+public class TimeImpl implements TimeInterface {
+
+    private long time=0;
+    private long currentTimeSystem;
+    Thread d;
+    public TimeImpl(){
+        time = 0;
+        currentTimeSystem = currentThreadTimeMillis();
+    }
+
+    @Override
+    public void startTime() {
+    time=currentThreadTimeMillis()-currentTimeSystem;
+    d =new Thread(new Runnable() {
+        @Override
+        public void run() {
+            time+=currentThreadTimeMillis();
+            System.out.println(time);
+
+        }
+    });
+    }
+
+    @Override
+    public long getTime() {
+        return time;
+    }
+}
